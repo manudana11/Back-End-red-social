92ff8f75dfe0e78716c63ef905e51aeb046de448
manudana11 <manueldanapallardo@gmail.com>
2024-05-12 19:58:06 +0200

    prototipo modelo posts creado

--- models/Post.js
+++ models/Post.js
@@ -1,16 +1,25 @@
 const mongoose = require('mongoose');
+const ObjectId = mongoose.SchemaType.ObjectId;
 
 const PostSchema = new mongoose.Schema({
-    userId: ObjectId,
-    title: String,
     imgpost: String,
-    date: Date,
-    taggedpeople: Array,
-    likes: Array,
-    comments: Array,
+    caption: String,
+    userId: [{type:ObjectId, ref:'User'}],
+    taggedpeople: [{type:ObjectId, ref:'Tagged'}],
+    likes: [{type:ObjectId, ref:'Likes'}],
+    comments: [{
+        userId: {type:ObjectId, ref:'User'},
+        bodyText: String,
+        likes: [{type:ObjectId, ref:'User'}],
+        responses: [{
+            userId: {type:ObjectId, ref:'User'},
+            bodyText: String,
+            likes: [{type:ObjectId, ref:'User'}],
+        }],
+    }],
     location: String,
 }, { timestamps: true });
 
-const Post = mongoose.model('User', PostSchema);
+const Post = mongoose.model('Post', PostSchema);
 
 module.exports = Post;
